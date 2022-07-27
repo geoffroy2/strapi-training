772b86146003d0b0313dcedd22a576ba390cf260
fleury93 <contact.geoffroy@gmail.com>
2022-07-27 14:18:38 +0000

    first commit

---
+++ src/api/categorie-post/controllers/categorie-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  categorie-post controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::categorie-post.categorie-post');
---
+++ src/api/categorie-post/services/categorie-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * categorie-post service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::categorie-post.categorie-post');
